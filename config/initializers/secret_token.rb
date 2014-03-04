5d7c81c9ab4b526fa26ec63d68065cc611d60f40
dlopez <daniellopezh@gmail.com>
2014-03-03 20:08:52 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TweetStalker::Application.config.secret_key_base = '629dcb8502e60de35bdefc191298ce1d25a7a11c181033b0307a87fabe03ec40064edb7d00ca0d299ffc550adf8c09d6d788cdf45b2eb75e0a65570e55de6eb0'
