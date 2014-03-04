45c84b2aa6252a6de430fcddfcec56db3ffb25cc
dlopez <daniellopezh@gmail.com>
2014-03-04 12:42:15 -0500

    fixed production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,7 @@ TweetStalker::Application.configure do
 
   config.serve_static_assets = true
 
-  config.action_dispatch.x_sendfile_header = ‘X-Accel-Redirect’
+  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
 
   config.assets.compile = true
 end
